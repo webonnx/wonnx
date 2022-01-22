2ed5bcb9dfc4ae40b9f8364c36688fe641448786
haixuanTao <hai-xuan.tao@student.ecp.fr>
2022-01-22 15:14:20 +0100

    fixing username problem

--- .github/deploy_manylinux.sh
+++ .github/deploy_manylinux.sh
@@ -3,4 +3,4 @@ rustup override set nightly-2022-01-01
 export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+popcnt'
 maturin publish \
   --skip-existing \
-  --username haixuantao
+  --username __token__
