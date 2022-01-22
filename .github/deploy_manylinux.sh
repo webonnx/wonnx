64987ed1df18c143dee43c787640bf328b33655d
haixuanTao <hai-xuan.tao@student.ecp.fr>
2022-01-22 14:59:32 +0100

    Adding github workflow for pushing to pypi

---
+++ .github/deploy_manylinux.sh
@@ -0,0 +1,6 @@
+cd py-wonnx
+rustup override set nightly-2022-01-01
+export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+popcnt'
+maturin publish \
+  --skip-existing \
+  --username haixuantao
