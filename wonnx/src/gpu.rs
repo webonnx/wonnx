f419a97f86c77b607293df7001e6de8d7a1adb62
Tommy van der Vorst <tommy@pixelspark.nl>
2022-03-04 20:46:15 +0100

    fix: use an intermediate buffer to download output to CPU when compiling to WASM
    WebGPU on Chrome does not allow MAP_READ with STORAGE buffer usage

--- wonnx/src/gpu.rs
+++ wonnx/src/gpu.rs
@@ -302,7 +302,9 @@ impl GpuModel {
                             InputTensor::I32(v) => v.iter().map(|f| (*f) as f32).collect(),
                         }
                     }
-                    InferenceOutput::Tensor(tensor) => tensor.read_to_vec(&self.device).await?,
+                    InferenceOutput::Tensor(tensor) => {
+                        tensor.read_to_vec(&self.device, &self.queue).await?
+                    }
                 },
             );
         }
@@ -333,7 +335,13 @@ impl TensorProtoExtra for TensorProto {
         };
 
         let buffer_usage = match readable {
-            true => BufferUsages::MAP_READ | BufferUsages::STORAGE,
+            true => {
+                if cfg!(target_arch = "wasm32") {
+                    BufferUsages::STORAGE | BufferUsages::COPY_SRC
+                } else {
+                    BufferUsages::STORAGE | BufferUsages::MAP_READ
+                }
+            }
             false => BufferUsages::STORAGE,
         };
 
@@ -390,7 +398,11 @@ impl<'model> OperatorDefinition<'model> {
                 );
 
                 let buffer_usage = if outputs_readable {
-                    BufferUsages::STORAGE | BufferUsages::MAP_READ
+                    if cfg!(target_arch = "wasm32") {
+                        BufferUsages::STORAGE | BufferUsages::COPY_SRC
+                    } else {
+                        BufferUsages::STORAGE | BufferUsages::MAP_READ
+                    }
                 } else {
                     BufferUsages::STORAGE
                 };
@@ -544,12 +556,25 @@ impl GpuStep {
 
 impl GpuTensor {
     /// Read the tensor from GPU memory to main memory (as Vec<f32>)
-    async fn read_to_vec(&self, device: &wgpu::Device) -> Result<Vec<f32>, GpuError> {
+    async fn read_to_vec(
+        &self,
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+    ) -> Result<Vec<f32>, GpuError> {
         let buffer_slice = self.buffer.slice(..);
-        let buffer_future = buffer_slice.map_async(wgpu::MapMode::Read);
-        device.poll(wgpu::Maintain::Wait);
-        buffer_future.await.expect("failed to run compute on gpu!");
-        let output_data = buffer_slice.get_mapped_range();
+
+        #[cfg(target_arch = "wasm32")]
+        let output_data = wgpu::util::DownloadBuffer::read_buffer(device, queue, &buffer_slice)
+            .await
+            .unwrap();
+
+        #[cfg(not(target_arch = "wasm32"))]
+        let output_data = {
+            let buffer_future = buffer_slice.map_async(wgpu::MapMode::Read);
+            device.poll(wgpu::Maintain::Wait);
+            buffer_future.await.expect("failed to run compute on gpu!");
+            buffer_slice.get_mapped_range()
+        };
 
         // The actual buffer may be bigger than what we should return, because buffers have a minimum size in wgpu
         // Fetch the size we should expect so we can chop the buffer to the correct size
