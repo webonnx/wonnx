e5287f98b7c74b0ff92ec03c643a11e601d968ef
haixuanTao <hai-xuan.tao@student.ecp.fr>
2022-03-16 13:34:50 +0100

    Fixing CI

--- .github/test_onnx_backend.sh
+++ .github/test_onnx_backend.sh
@@ -3,6 +3,7 @@ rustup override set nightly-2022-01-01
 export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+popcnt'
 python3 -m venv .env
 source .env/bin/activate
+pip install --upgrade pip
 pip install maturin
 pip install -r requirements.txt
 maturin develop
