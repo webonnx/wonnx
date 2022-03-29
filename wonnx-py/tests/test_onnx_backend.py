84176e69a8e10736d9fffdf1f987b7eb06114f40
Tommy van der Vorst <tommy@pixelspark.nl>
2022-03-29 23:24:09 +0200

    fix: SyntaxError: from __future__ imports must occur at the beginning of the file

--- wonnx-py/tests/test_onnx_backend.py
+++ wonnx-py/tests/test_onnx_backend.py
@@ -1,11 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from __future__ import absolute_import
-import numpy as np
-import wonnx
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+
+import numpy as np
+import wonnx
 
 import itertools
 import os
