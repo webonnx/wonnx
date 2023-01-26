09484327a0a90f42de8d09930f16b7d44c7bb58e
CameronTofer <cameron@beamdog.com>
2023-01-25 18:42:01 -0700

    implement LRN

---
+++ wonnx/templates/matrix/lrn.wgsl
@@ -0,0 +1,23 @@
+{%- include "structs.wgsl" -%}
+
+@group(0) @binding(0)
+var<storage, read> input_0: Array;
+
+@group(0) @binding(1)
+var<storage, read_write> output_0: Array;
+
+@compute @workgroup_size({{ workgroup_size_x }})
+fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+    let c = global_id.x;
+    //let chunk_start = {{ i_chunks[0][1] }}u * c;
+    let start = (c / {{ i_shape[0][1] }}u) * {{ i_shape[0][1] }}u;
+    let end = start + {{ i_shape[0][1] - 1 }}u;
+
+    var square_sum: Scalar = Scalar();
+    for (var i = max(start, c - {{left_size}}u); i <= min(end, c + {{right_size}}u); i++) {
+        let I = input_0.data[i];
+        square_sum += I * I;
+    }
+
+    output_0.data[c] = input_0.data[ c ] / pow({{bias}} + ({{alpha}} / {{size}}.0) * square_sum,{{beta}});
+}
---
+++ wonnx/tests/localresponsenormalization.rs
@@ -0,0 +1,52 @@
+use std::{collections::HashMap, convert::TryInto};
+use wonnx::utils::{attribute, graph, model, node, tensor};
+mod common;
+
+#[test]
+fn local_response_normalization() {
+    let mut input_data = HashMap::new();
+
+    let batches = 1;
+    let width_height: usize = 3;
+    let channels: usize = 4;
+    let data: Vec<f32> = [ 1.,1.,2.,4.,  2.,2.,1.,2., 3.,1.,2.,1., 4.,2.,3.,5., 3.,3.,2.,2., 6.,2.,3.,1., 7.,3.,4.,2., 8.,4.,3.,2., 9.,3.,4.,4.].to_vec();
+
+    let shape = vec![
+        batches as i64,
+        channels as i64,
+        width_height as i64,
+        width_height as i64,
+    ];
+    input_data.insert("X".to_string(), data.as_slice().into());
+
+    let bn_model = model(graph(
+        vec![tensor("X", &shape)],  // input
+        vec![tensor("Y", &shape)],  // output
+        vec![],                     // infos
+        vec![],                     // intializers
+        
+        // nodes
+        vec![node(
+            vec!["X"],
+            vec!["Y"],
+            "lrn",
+            "LocalResponseNormalization",
+            vec![   attribute("alpha", 1.0),
+                    attribute("beta",  1.0),
+                    attribute("bias",  0.0),
+                    attribute("size",  2)],
+        )],
+    ));
+
+    // LOGIC
+    let session =
+        pollster::block_on(wonnx::Session::from_model(bn_model)).expect("Session did not create");
+
+    let result = pollster::block_on(session.run(&input_data)).unwrap();
+    let out_y = &result["Y"];
+
+    common::assert_eq_vector(
+        out_y.try_into().unwrap(),
+        &[1.0, 0.4, 0.2, 0.5, 0.5, 0.8, 0.4, 1.0, 0.6, 0.4, 0.8, 2.0, 0.4, 0.30769232, 0.1764706, 0.39999998, 0.33333334, 0.4615385, 0.5, 1.0, 0.3, 0.30769232, 0.6, 2.0, 0.2413793, 0.24, 0.4, 1.0, 0.2, 0.32, 0.4615385, 1.0, 0.2, 0.24, 0.25, 0.5],
+    );
+}
