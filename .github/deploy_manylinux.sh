e8df736cbffb34f2eab3f44ab66f4d4e166790d6
Tommy van der Vorst <tommy@pixelspark.nl>
2023-04-20 20:44:12 +0200

    chore: fix Python release

--- .github/deploy_manylinux.sh
+++
@@ -1,6 +0,0 @@
-cd wonnx-py
-rustup override set nightly-2022-01-01
-export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+popcnt'
-maturin publish \
-  --skip-existing \
-  --username __token__
